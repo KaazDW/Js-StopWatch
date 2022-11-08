4682df5b3658b2b96755cc41acc300f17f3f757f
Kaaz <92046563+KaazDW@users.noreply.github.com>
2022-11-08 11:06:08 +0100

    update responsive not functionnal

--- app.js
+++ app.js
@@ -32,8 +32,8 @@ function timerReset(){
 }
 function startFunction(){
     msec++;
-    if(msec < 10) msec = "0" + msec;
-    // if(sec <= 9) sec = "0" + sec;
+    if(msec < 10) msec = "0" + msec; console.log(msec);
+    if(sec <= 9) sec = "0" + sec;
 
     if(msec > 100) {
         sec++;
